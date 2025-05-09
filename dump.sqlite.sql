5365ce37b1416178ee9c54e5a418c9d34c05f2b3
Igor Raposo <igorraposo02@gmail.com>
2025-05-09 13:59:46 +0100

    atualização

---
+++ dump.sqlite.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'events' ('c1' TEXT,'c2' TEXT, is_deleted INTEGER DEFAULT 0);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
